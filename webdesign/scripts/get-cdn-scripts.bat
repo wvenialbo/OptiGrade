5931ebf9be1471c77c40bc0fbd86245e40a9fcc5
Waldemar Villamayor-Venialbo <wvenialbo@gmail.com>
2014-05-11 10:08:51 -0400

    Added website layout HTML templates

---
+++ webdesign/scripts/get-cdn-scripts.bat
@@ -0,0 +1,32 @@
+:: <!-- Latest compiled and minified CSS -->
+:: <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+wget -c --no-check-certificate https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css
+
+:: <!-- Optional theme -->
+:: <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+wget -c --no-check-certificate https://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css
+
+:: <!-- Latest compiled and minified JavaScript -->
+:: <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+wget -c --no-check-certificate https://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js
+
+:: <!-- HTML5 Shim IE8 support of HTML5 elements -->
+:: <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+wget -c --no-check-certificate https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js
+
+:: <!-- Respond.js IE8 support of media queries -->
+:: <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+wget -c --no-check-certificate https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js
+
+:: <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+:: <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+wget -c --no-check-certificate https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
+
+:: <!-- reCAPTCHA Ajax API-->
+wget -c --no-check-certificate http://www.google.com/recaptcha/api/js/recaptcha_ajax.js
+
+:: CHECK ALSO
+:: http://twitter.github.io/typeahead.js/
+:: http://bootply.com/
+
+pause
